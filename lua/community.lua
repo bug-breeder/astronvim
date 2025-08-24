3fc7297b4ce1d458b21c3cec3ec91b343723be7b
Alan Nguyen <anhngw@gmail.com>
2025-08-24 10:59:50 +0700

    feat: add language packs

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -7,6 +5,15 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.c" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.javascript" },
+  { import = "astrocommunity.pack.html" },
+  { import = "astrocommunity.pack.css" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
 }
